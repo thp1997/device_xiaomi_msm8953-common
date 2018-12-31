ba39140b844d0c870606b4726360a0b77685944b
thp1997 <tirthpatel49@gmail.com>
2018-12-31 18:42:04 +0530

    msm8953-common: Rename persist.sys.usb.config.extra
    Fixes Tethering

--- prop.mk
+++ prop.mk
@@ -190,7 +190,7 @@ net.tcp.2g_init_rwnd=10
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config.extra=none
+persist.vendor.usb.config.extra=none
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
